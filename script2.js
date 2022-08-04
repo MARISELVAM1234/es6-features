ebe06c58c998ceedb3746986703d2cc1e655cf30
selvam <selvamplkm111@gmail.com>
2022-08-04 23:06:29 +0530

    update-script2.

--- script2.js
+++ script2.js
@@ -14,7 +14,7 @@
 //             let a = 23 ;
 //             let b = 10 ;
 //             console.log("sub : " , a-b);
-//             resolve({'age' : 20})
+//             reject({'age' : 20})
 //         },3000);
 //     });
 // }
@@ -33,8 +33,10 @@
 // const p3 = ()=>{
 //     new Promise((resolve, reject) => {
 //         setTimeout(()=>{
+//             console.log('welcome');
 //             resolve()
 //         });
+       
 //     });
 // }
 // Promise.all ([p1,p2, p3])
@@ -48,27 +50,49 @@
 
 
 //promises
+// let add = (data) =>{
+//     return new Promise((resolve, reject) => {
+//         setTimeout(()=>{
+//             console.log('add');
+//             resolve({'name': 'react'});
+//         },4000);
+//     });
+// }
+// let sub =(data2) =>{
+//     return new Promise((resolve, reject) => {
+//         console.log('sub');
+//         resolve({'num' : 23})
+//     });
+// }
 
-let add = (data) =>{
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            console.log('add');
-            resolve({'name': 'react'});
-        },4000);
-    });
-}
-let sub =(data2) =>{
-    return new Promise((resolve, reject) => {
-        console.log('sub');
-        resolve({'num' : 23})
-    });
-}
+// let final =async () =>{
+// const val = await add()
+// const val2 = sub()
+// console.log('val' , val);
+// console.log('overall' , val , val2 );
+// sub()
+// }
+// final()
 
-let final =async () =>{
-const val = await add()
-const val2 = sub()
-console.log('val' , val);
-console.log('overall' , val , val2 );
-sub()
-}
-final()
+
+// //spread operator- concatenate the two function
+// let g1 = [1,23,4,5,13] 
+// let g2 = ['hai','hello']
+// let g3 = [...g1 , ...g2]
+// console.log(g3);
+
+
+
+// const p1 = Promise.resolve(3);
+// const p2 = 1337;
+// const p3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("foo");
+//   }, 100);
+// });
+
+// Promise.all([p1, p2, p3]).then((values) => {
+//   console.log(values); // [3, 1337, "foo"]
+// })
+// .catch(console.log('error'))
+
